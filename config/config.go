592a2f50224395fce6ec25134ea8e92ded9b98fa
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-07-04 00:03:56 +0200

    Manually rebase aggregation work on top of master

--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import "time"
+
 type NodeConfig struct {
-	P2P P2PConfig
+	P2P        P2PConfig
+	Aggregator bool
+	BlockTime  time.Duration
+	DALayer    string
+	DAConfig   []byte
 }
--- node/node.go
+++ node/node.go
@@ -3,6 +3,7 @@ package node
 import (
 	"context"
 	"fmt"
+	"time"
 
 	abci "github.com/lazyledger/lazyledger-core/abci/types"
 	llcfg "github.com/lazyledger/lazyledger-core/config"
@@ -11,21 +12,26 @@ import (
 	"github.com/lazyledger/lazyledger-core/libs/service"
 	corep2p "github.com/lazyledger/lazyledger-core/p2p"
 	"github.com/lazyledger/lazyledger-core/proxy"
-	"github.com/lazyledger/lazyledger-core/types"
+	lltypes "github.com/lazyledger/lazyledger-core/types"
 	"github.com/libp2p/go-libp2p-core/crypto"
+	"go.uber.org/multierr"
 
 	"github.com/lazyledger/optimint/config"
+	"github.com/lazyledger/optimint/da"
+	"github.com/lazyledger/optimint/da/registry"
+	"github.com/lazyledger/optimint/hash"
 	"github.com/lazyledger/optimint/mempool"
 	"github.com/lazyledger/optimint/p2p"
 	"github.com/lazyledger/optimint/store"
+	"github.com/lazyledger/optimint/types"
 )
 
 type Node struct {
 	service.BaseService
-	eventBus *types.EventBus
+	eventBus *lltypes.EventBus
 	proxyApp proxy.AppConns
 
-	genesis *types.GenesisDoc
+	genesis *lltypes.GenesisDoc
 
 	conf config.NodeConfig
 	P2P  *p2p.Client
@@ -37,19 +43,21 @@ type Node struct {
 
 	BlockStore store.Store
 
+	dalc da.DataAvailabilityLayerClient
+
 	// keep context here only because of API compatibility
 	// - it's used in `OnStart` (defined in service.Service interface)
 	ctx context.Context
 }
 
-func NewNode(ctx context.Context, conf config.NodeConfig, nodeKey crypto.PrivKey, clientCreator proxy.ClientCreator, genesis *types.GenesisDoc, logger log.Logger) (*Node, error) {
+func NewNode(ctx context.Context, conf config.NodeConfig, nodeKey crypto.PrivKey, clientCreator proxy.ClientCreator, genesis *lltypes.GenesisDoc, logger log.Logger) (*Node, error) {
 	proxyApp := proxy.NewAppConns(clientCreator)
 	proxyApp.SetLogger(logger.With("module", "proxy"))
 	if err := proxyApp.Start(); err != nil {
 		return nil, fmt.Errorf("error starting proxy app connections: %w", err)
 	}
 
-	eventBus := types.NewEventBus()
+	eventBus := lltypes.NewEventBus()
 	eventBus.SetLogger(logger.With("module", "events"))
 	if err := eventBus.Start(); err != nil {
 		return nil, err
@@ -60,6 +68,15 @@ func NewNode(ctx context.Context, conf config.NodeConfig, nodeKey crypto.PrivKey
 		return nil, err
 	}
 
+	dalc := registry.GetClient(conf.DALayer)
+	if dalc == nil {
+		return nil, fmt.Errorf("couldn't get data availability client named '%s'", conf.DALayer)
+	}
+	err = dalc.Init(conf.DAConfig, logger)
+	if err != nil {
+		return nil, fmt.Errorf("data availability layer client initialization error: %w", err)
+	}
+
 	mp := mempool.NewCListMempool(llcfg.DefaultMempoolConfig(), proxyApp.Mempool(), 0)
 
 	node := &Node{
@@ -68,6 +85,7 @@ func NewNode(ctx context.Context, conf config.NodeConfig, nodeKey crypto.PrivKey
 		genesis:      genesis,
 		conf:         conf,
 		P2P:          client,
+		dalc:         dalc,
 		Mempool:      mp,
 		mempoolIDs:   newMempoolIDs(),
 		incomingTxCh: make(chan *p2p.Tx),
@@ -104,9 +122,7 @@ func (n *Node) mempoolPublishLoop(ctx context.Context) {
 
 	for {
 		// wait for transactions
-		n.Logger.Debug("loop begin")
 		if next == nil {
-			n.Logger.Debug("waiting for mempool")
 			select {
 			case <-rawMempool.TxsWaitChan():
 				if next = rawMempool.TxsFront(); next != nil {
@@ -119,7 +135,6 @@ func (n *Node) mempoolPublishLoop(ctx context.Context) {
 
 		// send transactions
 		for {
-			n.Logger.Debug("Gossiping...")
 			memTx := next.Value.(*mempool.MempoolTx)
 			tx := memTx.Tx
 
@@ -136,7 +151,6 @@ func (n *Node) mempoolPublishLoop(ctx context.Context) {
 			next = nx
 		}
 
-		n.Logger.Debug("waiting for next...")
 		select {
 		case <-next.NextWaitChan():
 			next = next.Next()
@@ -146,14 +160,99 @@ func (n *Node) mempoolPublishLoop(ctx context.Context) {
 	}
 }
 
+func (n *Node) aggregationLoop(ctx context.Context) {
+	tick := time.NewTicker(n.conf.BlockTime)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-tick.C:
+			err := n.publishBlock(ctx)
+			if err != nil {
+				n.Logger.Error("error while publishing block", "error", err)
+			}
+		}
+	}
+}
+
+func (n *Node) publishBlock(ctx context.Context) error {
+	n.Logger.Info("Creating and publishing block")
+
+	// TODO(tzdybal): use block executor here
+	var err error
+	var block *types.Block
+	var commit *types.Commit
+
+	err = n.BlockStore.SaveBlock(block, commit)
+	if err != nil {
+		return err
+	}
+	return n.broadcastBlock(ctx, block)
+}
+
+func (n *Node) makeBlock(height uint64, txs types.Txs) (*types.Block, error) {
+	// TODO(tzdybal): fill all fields
+	lastBlock, err := n.BlockStore.LoadBlock(height - 1)
+	if err != nil {
+		return nil, err
+	}
+	lastHash, err := hash.Hash(&lastBlock.Header)
+	if err != nil {
+		return nil, err
+	}
+
+	block := &types.Block{
+		Header: types.Header{
+			Version: types.Version{
+				Block: 0,
+				App:   0,
+			},
+			NamespaceID:     [8]byte{},
+			Height:          height,
+			Time:            uint64(time.Now().UnixNano()), // TODO(tzdybal): how to get TAI64?
+			LastHeaderHash:  lastHash,
+			LastCommitHash:  [32]byte{},
+			DataHash:        [32]byte{},
+			ConsensusHash:   [32]byte{},
+			AppHash:         [32]byte{},
+			LastResultsHash: [32]byte{},
+			ProposerAddress: nil,
+		},
+		Data: types.Data{
+			Txs:                    txs,
+			IntermediateStateRoots: types.IntermediateStateRoots{RawRootsList: nil},
+			Evidence:               types.EvidenceData{Evidence: nil},
+		},
+		// LastCommit: nil, // TODO(tzdybal)
+	}
+
+	block.Header.DataHash, err = hash.Hash(&block.Data)
+	if err != nil {
+		return nil, err
+	}
+
+	return block, nil
+}
+
+func (n *Node) broadcastBlock(ctx context.Context, block *types.Block) error {
+	return nil
+}
+
 func (n *Node) OnStart() error {
 	n.Logger.Info("starting P2P client")
 	err := n.P2P.Start(n.ctx)
 	if err != nil {
 		return fmt.Errorf("error while starting P2P client: %w", err)
 	}
+	err = n.dalc.Start()
+	if err != nil {
+		return fmt.Errorf("error while starting data availability layer client: %w", err)
+	}
 	go n.mempoolReadLoop(n.ctx)
 	go n.mempoolPublishLoop(n.ctx)
+	if n.conf.Aggregator {
+		go n.aggregationLoop(n.ctx)
+	}
 	n.P2P.SetTxHandler(func(tx *p2p.Tx) {
 		n.incomingTxCh <- tx
 	})
@@ -162,7 +261,9 @@ func (n *Node) OnStart() error {
 }
 
 func (n *Node) OnStop() {
-	n.P2P.Close()
+	err := n.dalc.Stop()
+	err = multierr.Append(err, n.P2P.Close())
+	n.Logger.Error("errors while stopping node: %w", err)
 }
 
 func (n *Node) OnReset() error {
@@ -177,7 +278,7 @@ func (n *Node) GetLogger() log.Logger {
 	return n.Logger
 }
 
-func (n *Node) EventBus() *types.EventBus {
+func (n *Node) EventBus() *lltypes.EventBus {
 	return n.eventBus
 }
 
